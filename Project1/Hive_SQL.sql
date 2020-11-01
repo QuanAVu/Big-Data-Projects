2b19eb8619d42e43c7697f93dd3953e3d4b56276
unknown <quan.vu@revature.net>
2020-10-31 22:20:57 -0700

    Added query 2

--- Project1/Hive_SQL.sql
+++ Project1/Hive_SQL.sql
@@ -1,4 +1,22 @@
---Query 1
+-- Quan Vu, All SQL queries needed for HIVE in project 1
+
+-- Create new table, here is an example:
+CREATE TABLE CLICKSTREAM
+(ORIGIN STRING, INTERNAL STRING, RELATION STRING, CLICKS INT)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t';
+-- Load local data into table 
+LOAD DATA LOCAL INPATH '/home/quanvu/Project1-Data/clickstream-enwiki-2020-09.tsv' INTO TABLE clickstream;
+
+-- Query 1
+/*	Get the top 10 highest viewed English wikipedia pages.
+	First sum is the summing of count views from all 24 hours
+	page_view files.
+	Second sum is summing matching articles from both en and en.m */
+-- Can add the query result onto HDFS: 
+INSERT OVERWRITE DIRECTORY '/user/hive/output/Query1'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '  '
 SELECT NewTable.PAGE_TITLE, SUM(NewTable.SUM_VIEWS) AS TOTAL_VIEWS
 FROM (SELECT DOMAIN_CODE, PAGE_TITLE, SUM(COUNT_VIEWS) AS SUM_VIEWS
       FROM PAGE_VIEW
@@ -7,3 +25,13 @@ FROM (SELECT DOMAIN_CODE, PAGE_TITLE, SUM(COUNT_VIEWS) AS SUM_VIEWS
 GROUP BY NewTable.PAGE_TITLE
 ORDER BY TOTAL_VIEWS DESC
 LIMIT 10;
+
+-- Query 2
+/*	Get the top 10 English wikipedia articles that have internal links
+	that were clicked the most.*/
+SELECT * 
+FROM CLICKSTREAM
+WHERE relation='link'
+ORDER BY clicks DESC
+LIMIT 10;
+
