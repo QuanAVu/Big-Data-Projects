1ed3cd0459d047722ec59b4016f97650185a4d83
unknown <quan.vu@revature.net>
2020-11-02 22:53:10 -0800

    Revised Query 2 and 3

--- Project1/Hive_SQL.sql
+++ Project1/Hive_SQL.sql
@@ -1,6 +1,16 @@
 -- Quan Vu, All SQL queries needed for HIVE in project 1
+-- The queries are written in a format that works for Hive beeline
+-- so it is a little messy and hard to read.
 
--- Create new table, here is an example:
+-- Tables created:
+
+CREATE TABLE PAGE_VIEW
+(DOMAIN_CODE STRING, PAGE_TITLE STRING, COUNT_VIEWS INT, TOTAL_RESPONSE_SIZE INT)
+ROW FORMAT DELIMITED	-- Each line represents a record
+FIELDS TERMINATED BY ' ' -- Each element for each column is separated by a space
+-- Load 
+LOAD DATA LOCAL INPATH '/home/quanvu/Project1-Data/pageViews/octo20-fullday' INTO TABLE PAGE_VIEW;
+
 CREATE TABLE CLICKSTREAM
 (ORIGIN STRING, INTERNAL STRING, RELATION STRING, CLICKS INT)
 ROW FORMAT DELIMITED
@@ -29,9 +39,42 @@ LIMIT 10;
 -- Query 2
 /*	Get the top 10 English wikipedia articles that have internal links
 	that were clicked the most.*/
-SELECT * 
-FROM CLICKSTREAM
-WHERE relation='link'
-ORDER BY clicks DESC
+INSERT OVERWRITE DIRECTORY '/user/hive/output/Query2'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '  '
+SELECT Table2.origin AS referrer, Table2.internal AS requested, ROUND(Table2.clicks/Table1.total1, 2) AS fraction
+FROM (SELECT origin, SUM(clicks) AS total1 FROM CLICKSTREAM WHERE relation='link' GROUP BY origin ORDER BY total1 DESC LIMIT 1000) AS Table1, (SELECT * FROM CLICKSTREAM WHERE relation='link' ORDER BY clicks DESC LIMIT 1000) AS Table2
+WHERE Table1.origin = Table2.origin
+ORDER BY fraction DESC 
 LIMIT 10;
+
+
+-- Query 3
+/* 	Get a series of articles starting from Hotel_California
+	that have the highest clicks (the clicks number is not
+	accurate because it is a combination of both clicks that are 
+	from the chain and clicks from when user search for the last
+	internal article as an original article)*/
+	/*
+SELECT NewTable.ORIGIN, NewTable.INTERNAL, CLICKSTREAM.INTERNAL AS INTERNAL2, CLICKSTREAM.CLICKS
+FROM CLICKSTREAM, (SELECT * 
+	FROM CLICKSTREAM 
+	WHERE RELATION='link' AND ORIGIN='Hotel_California' 
+	ORDER BY CLICKS DESC LIMIT 1) AS NewTable
+WHERE CLICKSTREAM.ORIGIN = NewTable.INTERNAL
+ORDER BY CLICKSTREAM.CLICKS DESC
+LIMIT 1;*/
+
+SELECT NewTable.referrer AS referrer, NewTable.requested AS requested1, CLICKSTREAM.internal AS requested2, ROUND(CLICKSTREAM.clicks/NewTable.total, 2) AS final_fraction
+FROM CLICKSTREAM, (SELECT Table2.origin AS referrer, Table2.internal AS requested, Table1.total1 AS total, ROUND(Table2.clicks/Table1.total1, 2) AS fraction
+FROM (SELECT origin, SUM(clicks) AS total1 FROM CLICKSTREAM WHERE origin='Hotel_California' AND relation='link' GROUP BY origin ORDER BY total1 DESC LIMIT 1000) AS Table1, (SELECT * FROM CLICKSTREAM WHERE origin='Hotel_California' AND relation='link' ORDER BY clicks DESC LIMIT 1000) AS Table2
+WHERE Table1.origin = Table2.origin
+ORDER BY fraction DESC LIMIT 1) AS NewTable 
+WHERE NewTable.requested = CLICKSTREAM.origin
+ORDER BY final_fraction DESC
+LIMIT 10;
+
+
+-- Query 4
+
 
